5457ebd0b87aeb04dd34a15a391a6bfb01a67d24
Liu Zhe <lzhe72537@gmail.com>
2024-04-21 03:38:05 -0400

    update invoice

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -14,6 +14,28 @@ const FormSchema = z.object({
 })
 
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
+
+const invoicesPath = '/dashboard/invoices';
+
+export async function updateInvoice(id: string, formData: FormData) {
+  const rawFormData = {
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  };
+  const { customerId, amount, status } = UpdateInvoice.parse(rawFormData);
+  const amountInCents = amount * 100;
+
+  await sql`
+    UPDATE invoices
+    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+    WHERE id = ${id}
+  `
+
+  revalidatePath(invoicesPath);
+  redirect(invoicesPath)
+}
  
 export async function createInvoice(formData: FormData) {
   const rawFormData = {
